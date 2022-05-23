1f4d7b31b3b18a91824079497a891374a9d60c9c
Diego Carvalho <diegodmca@outlook.com>
2022-05-23 10:16:52 -0300

    Initial Commit

---
+++ I Am Happy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Happy
+//
+//  Created by Diego Carvalho on 23/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
